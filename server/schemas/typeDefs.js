9b1b2f6fe7d329b76c26991fceb95639985722de
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-30 08:34:11 -0500

    Write typeDefs for items, lists, users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,17 +8,52 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
+    savedLists: [List]
+  }
+
+  type Item {
+    _id: ID
+    name: String!
+    quantity: Number
+    department: String
+  }
+
+  type List {
+    _id: ID
+    name: String!
+    savedItems: [Item]
   }
 
   type Query {
     me: User
     users: [User]
     user(username: String!): User
+    item(name: String!): Item
+    items: [Item]
+    list(name: String!): List
+    lists: [List]
+  }
+
+  input ItemInput {
+    _id: ID
+    name: String!
+    quantity: Number
+    department: String
+  }
+  
+  input ListInput {
+    _id: ID
+    name: String!
+    savedItems: [Item]
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addItem(itemData: ItemInput!): List
+    removeItem(name: String!): List
+    addList(listData: ListInput!): User 
+    removeList(name: String!): User 
   }
 
   type Auth {
